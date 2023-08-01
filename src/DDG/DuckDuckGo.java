07948288922d7b25d76266f64fb3479d801645af
Robin <rirobink@gmail.com>
2023-08-01 13:42:08 +0600

    asset and gamelogic update

---
+++ src/DDG/DuckDuckGo.java
@@ -0,0 +1,24 @@
+package DDG;
+
+
+
+import javax.swing.JFrame;
+
+public class DuckDuckGo extends JFrame{
+
+    public DuckDuckGo() {
+        add(new Model());
+    }
+
+
+    public static void main(String[] args) {
+        DuckDuckGo ddg = new DuckDuckGo();
+        ddg.setVisible(true);
+        ddg.setTitle("DuckDuckGo");
+        ddg.setSize(380,420);
+        ddg.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ddg.setLocationRelativeTo(null);
+
+    }
+
+}
